505dae3b4cb8381a369d33dedd8bcb83398beb38
000234768 <juan_lulavel@hotmail.com>
2024-05-12 00:18:54 -0400

    diseño final

--- examen_juan_lavado/lib/screen/home.dart
+++ examen_juan_lavado/lib/screen/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:examen_juan_lavado/screen/screen.dart';
-import 'package:examen_juan_lavado/routes/app_routes.dart'; // Importa el archivo donde defines tus rutas
+import 'package:examen_juan_lavado/screen/login_screen.dart'; // Asegúrate de que la ruta de importación es correcta
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,52 +8,82 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
-        automaticallyImplyLeading: true, // Esto mostrará el botón de retroceso en el AppBar
+        title: Text('Administracion mi tiendita'),
+        backgroundColor: Colors.blue,
+        automaticallyImplyLeading: false, // Esto elimina el botón de atrás
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () => Navigator.pushReplacementNamed(context, 'login'), // Reemplaza con tu ruta correcta
+          ),
+        ],
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
+          SizedBox(height: 20), // Espacio adicional para estética
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.spaceAround, // Espacio uniforme
               children: [
-                InkWell(
-                  onTap: () {
-                    Navigator.pushNamed(context, 'list');
-                  },
-                  child: Text('Productos'),
-                ),
-                Text('|'),
-                InkWell(
-                  onTap: () {
-                    Navigator.pushNamed(context, 'main'); 
-                  },
-                  child: Text('Categorías'),
-                ),
-                Text('|'),
-                InkWell(
-                  onTap: () {
-                    Navigator.pushNamed(context, 'provider_detail');
-                  },
-                  child: Text('Proveedores'),
-                ),
+                _navigationButton(context, 'Productos', Icons.shopping_cart, 'list'),
+                _navigationButton(context, 'Proveedores', Icons.people, 'provider_detail'),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _navigationButton(context, 'Categorías', Icons.category, 'main'),
               ],
             ),
           ),
           Expanded(
-            child: ListProductScreen(),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.network('https://latienditadigital.com/img/la-tiendita-digital-logo-1599862452.jpg', width: 200, height: 200),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      'Bienvenido a la administración de La Tiendita Digital. Aquí podrás gestionar productos, categorías y proveedores para mantener tu tienda actualizada.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.blueGrey, fontSize: 16),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 12),
+            color: Colors.deepPurple[50], // Color suave para el fondo del pie de página
             child: Text(
               '2024 Todos los derechos reservados Juan Lavado',
               textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 12),
+              style: TextStyle(color: Colors.blue, fontSize: 12),
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _navigationButton(BuildContext context, String title, IconData icon, String route) {
+    return ElevatedButton.icon(
+      icon: Icon(icon, color: Colors.white),
+      label: Text(title, style: TextStyle(color: Colors.white)),
+      onPressed: () => Navigator.pushNamed(context, route),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.blue,
+        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
     );
   }
